afaa14fb1e21da19aee8a565f35508ca8d9af509
coders Club <codersclub.com>
2025-03-06 12:58:42 +0300

    JwtWebToken added.

--- CleanArchitecture.WepAPI/Program.cs
+++ CleanArchitecture.WepAPI/Program.cs
@@ -7,6 +7,7 @@ using CleanArchitecture.Persistance.Context;
 using CleanArchitecture.Persistance.Repositories;
 using CleanArchitecture.Persistance.Services;
 using CleanArchitecture.WepAPI.Middleware;
+using CleanArchitecture.WepAPI.OptionsSetup;
 using FluentValidation;
 using GenericRepository;
 using MediatR;
@@ -25,6 +26,10 @@ builder.Services.AddTransient<ExceptionMiddleware>();
 builder.Services.AddScoped<IUnitOfWork>(cfr=>cfr.GetRequiredService<AppDbContext>());
 builder.Services.AddScoped<ICarRepository,CarRepository>();
 
+
+builder.Services.ConfigureOptions<JwtOptionsSetup>();
+builder.Services.ConfigureOptions<JwtBearerOptionsSetup>();
+builder.Services.AddAuthentication().AddJwtBearer();
 
 
 builder.Services.AddAutoMapper(typeof(CleanArchitecture.Persistance.AssemblyReference).Assembly);
